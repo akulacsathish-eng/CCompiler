80e4399a1137be8d18735cf70d28d01e58658743
akulacsathish-eng <akulac.sathish@gmail.com>
2025-11-03 15:39:11 +0000

    week12

--- Week12 /arraysAsFunctions.c
+++ Week12 /arraysAsFunctions.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-// Function to print 1D array using pointer
-void print1DArray(int *arr, int size) {
+
+void print1DArray(int size,int *arr) 
+{
     printf("1D Array elements:\n");
     for (int i = 0; i < size; i++) {
         printf("%d ", *(arr + i)); // Dereferencing pointer
@@ -10,10 +11,12 @@ void print1DArray(int *arr, int size) {
 }
 
 
-void print2DArray(int (*arr)[3], int rows, int cols) {
+void print2DArray(int rows, int cols,int (*arr)[cols])  // or void print2DArray(int arr[2][3], int rows, int cols)
+{
     printf("2D Array elements:\n");
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+        for (int j = 0; j < cols; j++) 
+        {
             printf("%d ", arr[i][j]);   //  or *(*(arr + i) + j)
         }
         printf("\n");
@@ -24,14 +27,14 @@ void print2DArray(int (*arr)[3], int rows, int cols) {
 int main() {
  
     int oneD[5] = {10, 20, 30, 40, 50};
-    print1DArray(oneD, 5);
+    print1DArray(5, oneD);
 
   
     int twoD[2][3] = {
         {1, 2, 3},
         {4, 5, 6}
     };
-    print2DArray(twoD, 2, 3);
+    print2DArray(2, 3, twoD);
 
     return 0;
 }
